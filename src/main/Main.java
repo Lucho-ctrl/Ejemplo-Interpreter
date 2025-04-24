83f2f0925fd9323dd473cd05e4e1df7b512a54bb
Lucho-ctrl <kipotosqui33@gmail.com>
2025-04-24 09:03:08 -0500

    Comprobacion mayor que 18 y respuesta sino lo es

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,41 +1,67 @@
 package main;
+import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
-public static void main(String[] args) {
-    Contexto contexto = new Contexto();
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Contexto contexto = new Contexto();
+        Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Comprueba mayoria de edad de Juan, Pedro y Luis");
+        System.out.println("Comprueba mayoria de edad de Juan, Pedro y Luis");
 
-    while (true) {
-        System.out.print(">... ");
-        String linea = scanner.nextLine().trim();
-        String[] partes = linea.split(" ");
+        while (true) {
+            System.out.print(">... ");
+            String linea = scanner.nextLine().trim();
+            String[] partes = linea.split(" ");
 
-        if (partes.length == 4 && partes[0].equalsIgnoreCase("si") && partes[2].equalsIgnoreCase("mayor")) {
-            String nombreVar = partes[1];
-            
-            int valor = Integer.parseInt(partes[3]);
-            Expresion expr = new ExpresionMayorQue(nombreVar, valor);
-            boolean resultado = expr.interpretar(contexto);
+            //if (partes.length == 4 && partes[0].equalsIgnoreCase("si") && partes[2].equalsIgnoreCase("mayor")) {
+            //    String nombreVar = partes[1];
+//
+            //    int valor = Integer.parseInt(partes[3]);
+            //    Expresion expr = new ExpresionMayorQue(nombreVar, valor);
+            //    boolean resultado = expr.interpretar(contexto);
+//
+            //    if (resultado) {
+            //        System.out.println("✅ TRUE, puedes continuar. Escribe 'acceso permitido' para entrar.");
+//
+            //        String respuesta = scanner.nextLine().trim();
+            //        if (respuesta.equalsIgnoreCase("acceso permitido")) {
+            //            System.out.println("🎉 Bienvenido al bar 🥂");
+            //        } else {
+            //            System.out.println("⛔ Acceso cancelado.");
+            //        }
+//
+            //    } else {
+            //        System.out.println("❌ FALSE, largo.");
+            //    }            
+            //} 
+            if (partes.length > 3 && partes[0].equalsIgnoreCase("si") && partes[2].equalsIgnoreCase("mayor") 
+                && partes[4].equalsIgnoreCase("sino")) {
+                    String nombrePersona = partes[1];
 
-            if (resultado) {
-                System.out.println("✅ TRUE, puedes continuar. Escribe 'acceso permitido' para entrar.");
-                
-                String respuesta = scanner.nextLine().trim();
-                if (respuesta.equalsIgnoreCase("acceso permitido")) {
-                    System.out.println("🎉 Bienvenido al bar 🥂");
-                } else {
-                    System.out.println("⛔ Acceso cancelado.");
-                }
+                    int edadPersona = Integer.parseInt(partes[3]);
+                    Expresion expr = new ExpresionMayorQue(nombrePersona, edadPersona);
+                    boolean resultado = expr.interpretar(contexto);
 
+                    if(resultado) {
+                        System.out.println("✅ TRUE, puedes continuar. Escribe 'acceso permitido' para entrar.");
+
+                        String respuesta = scanner.nextLine().trim();
+                        if (respuesta.equalsIgnoreCase("acceso permitido")) {
+                            System.out.println("🎉 Bienvenido al bar 🥂");
+                        }
+
+                    } else if(!resultado) {
+                        int posicionInicial = 5; // Posición inicial del subarreglo
+                        String[] subarreglo = Arrays.copyOfRange(partes, posicionInicial, partes.length);
+                        String rtaMenorEdad = String.join(" ", subarreglo);
+                        System.out.println("FALSO "+rtaMenorEdad);
+                    }
             } else {
-                System.out.println("❌ FALSE, largo.");
+                System.out.println("Algo escribiste mal el formato es 'Si nombre mayor 18' ");
             }
-                
-        } else {
-            System.out.println("Algo escribiste mal el formato es 'Si nombre mayor 18' ");
-            }
-        }
+            
+
+        } 
     }
 }
+
